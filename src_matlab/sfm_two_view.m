8f7074220ff84cebd3b6cfb669504486933b1d29
Suryansh Kumar <k.sur46@gmail.com>
2022-06-04 14:15:54 +0200

    adding matlab example for two view

---
+++ src_matlab/sfm_two_view.m
@@ -0,0 +1,35 @@
+%Author: Suryansh Kumar, ETH
+
+% basic steps for two-view structure from motion
+
+%% step 1: read the two image and K matrix.
+I_ref = imread("../images/15.pgm");
+I_nex = imread("../images/19.pgm");
+K_mat = load("K_matrix.txt");
+
+%% step 2: compute the key point in the two images.
+corner_ref = detectHarrisFeatures(I_ref);
+corner_nex = detectHarrisFeatures(I_nex);
+[feature_ref, valid_ref] = extractFeatures(I_ref, corner_ref);
+[feature_nex, valid_nex] = extractFeatures(I_nex, corner_nex);
+
+figure,
+subplot(1, 2, 1); imshow(I_ref); hold on; 
+plot(valid_ref.Location(:, 1), valid_ref.Location(:, 2), 'ro');
+subplot(1, 2, 2), imshow(I_nex); hold on; 
+plot(valid_nex.Location(:, 1), valid_nex.Location(:, 2), 'g+');
+
+%% step 3: match the key points.
+indexPairs = matchFeatures(feature_ref, feature_nex);
+matchedpoint_ref = valid_ref(indexPairs(:, 1), :);
+matchedpoint_nex = valid_nex(indexPairs(:, 2), :);
+
+figure, 
+showMatchedFeatures(I_ref, I_nex, matchedpoint_ref, matchedpoint_nex);
+
+%% step 4: triangulate point.
+reconstructed_3d_points = reconstruct_3d_points(...
+    matchedpoint_ref.Location, matchedpoint_nex.Location, K_mat);
+
+
+
