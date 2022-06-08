e80a32e93eed80efc1393a442e57ed365d6fda72
Suryansh Kumar <k.sur46@gmail.com>
2022-06-08 11:13:39 +0200

    matlab main file commented

--- src_matlab/two_view_sfm.m
+++ src_matlab/two_view_sfm.m
@@ -1,25 +1,23 @@
-%Author: Suryansh Kumar, ETH
+% Author: Suryansh Kumar, ETH
 
 % basic steps for two-view structure from motion
 addpath(genpath("./mvg_modules"));
 
-% % step 1: read the two image and K matrix.
-% I_ref = imread("../images/IMG_2348.JPG");
-% I_nex = imread("../images/IMG_2349.JPG");
+% % step 1: read the two image and initialize the K matrix.
 I_ref = imread("../images/0022.JPG");
 I_nex = imread("../images/0023.JPG");
 
-% MATLAB inbuild feature descriptor match may fail at original image 
-% resolution. Thus, we resized it and appropritely changed the K matrix.
-% [m, n, ~] = size(I_ref);
-% im_scale = 4.0;
-% 
-% I_ref = imresize(I_ref, [m/im_scale, n/im_scale]);
-% I_nex = imresize(I_nex, [m/im_scale, n/im_scale]);
-% fx = 3838.27/im_scale; fy = 3837.22/im_scale; 
-% cx = 2808.00/im_scale; cy = 1872.00/im_scale;
-fx = 1698.873755; fy = 1698.8796645;
-cx = 971.7497705; cy = 647.7488275;
+% Note: MATLAB inbuild feature descriptor match may fail at very high image 
+% resolution. Thus, we may have to resized it and appropritely change
+% the K matrix values. For example: on IMG_2348.JPG IMG_2349.JPG, 
+% IMG_2356.JPG, IMG_2357.JPG choose im_div_scale = 4.0;
+
+[m, n, ~] = size(I_ref);
+im_div_scale = 1.0;
+I_ref = imresize(I_ref, [m/im_div_scale, n/im_div_scale]);
+I_nex = imresize(I_nex, [m/im_div_scale, n/im_div_scale]);
+fx = 1698.873755/im_div_scale; fy = 1698.8796645/im_div_scale;
+cx = 971.7497705/im_div_scale; cy = 647.7488275/im_div_scale;
 K_mat = [fx, 0, cx; 0, fy, cy; 0, 0, 1];
 
 % % step 2: compute the key point in the two images.
@@ -34,7 +32,7 @@ plot(valid_ref.Location(:, 1), valid_ref.Location(:, 2), 'r.');
 subplot(1, 2, 2), imshow(I_nex); hold on; 
 plot(valid_nex.Location(:, 1), valid_nex.Location(:, 2), 'g+');
 
-% step 3: match the key points.
+% % step 3: match the key points.
 indexPairs = matchFeatures(feature_ref, feature_nex);
 matchedpoint_ref = valid_ref(indexPairs(:, 1), :);
 matchedpoint_nex = valid_nex(indexPairs(:, 2), :);
@@ -47,13 +45,14 @@ showMatchedFeatures(I_ref, I_nex, matchedpoint_ref, matchedpoint_nex, ...
  [R, t, reconstructed_3d_points] = reconstruct_3d_points(...
      matchedpoint_ref.Location, matchedpoint_nex.Location, K_mat);
 
-
 % % step 5: plot the reconstruction.
-% MATLAB 3D plotting sucks big-time, so better store the 
+% MATLAB 3D visualizer sucks big-time, so better store the 
 % reconstructed_3d_points and visualize the points in a 
-% good 3D visualizer. Recommended software: pangolin.
+% good 3D visualizer. Recommended software: pangolin, open3d.
+
 %figure, hold on;
-%plot3(reconstructed_3d_points(:, 1), reconstructed_3d_points(:, 2), reconstructed_3d_points(:, 3), 'k.');
+% plot3(reconstructed_3d_points(:, 1), reconstructed_3d_points(:, 2), ...
+%    reconstructed_3d_points(:, 3), 'k.');
 
 
 
